bf48296702c2d479bf57faf262cd2392f68357ed
giovankes <hello@giotje.dev>
2023-12-28 16:27:53 +0100

    server now responds a 200 HTTP code indicated the connection was received successfully

--- src/main.rs
+++ src/main.rs
@@ -38,5 +38,9 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
     println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
