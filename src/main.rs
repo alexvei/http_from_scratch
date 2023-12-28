e31519019c19ddebcd99dd12321279ac9887a3e1
giovankes <hello@giotje.dev>
2023-12-28 16:18:12 +0100

    implemented basic http server that can listen to incoming requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+/* simple HTTP server */
+/* author: Giovanni */
+
+/* reasoning: understanding HTTP better */
+
+use std::net::TcpListener;
+
 fn main() {
-  println!("Hello, world!");
+    /* creating a local tcplistener at port 8477 */
+    const HOST: &str = "127.0.0.1";
+    const PORT: &str = "8477";
+
+    /* concat host address and port for final end point*/
+    let end_point: String = HOST.to_owned() + ":" + PORT;
+
+    /* create a tcp listener at our end point */
+    let listener = TcpListener::bind(end_point).unwrap();
+
+    /* let developer know that the web server is listening */
+    println!("web server is listening at port {}", PORT);
+
+    /* communicate with any incoming connections */
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("connection established!")
+    }
 }
